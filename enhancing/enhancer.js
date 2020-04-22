3bbf4082422e45664fb41b7d713f59e10ce7e3a4
nick migel <nickmigel2@gmail.com>
2020-04-22 15:59:24 -0400

    finished :D

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,39 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    item.enhancement = item.enhancement + 1
+    return { ...item }
+  } else {
+    return { ...item }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5
+    return { ...item }
+  } else if (item.enhancement >= 15) {
+    item.durability = item.durability - 10
+    if (item.enhancement > 15) {
+      item.enhancement = item.enhancement - 1
+      return { ...item }
+    } else {
+      return { ...item }
+    }
+  }
 }
 
 function repair(item) {
+  item.durability = 100
   return { ...item };
 }
 
 function get(item) {
-  return { ...item };
+  if (item.enhancement === 0) {
+    return { ...item }
+  } else {
+    item.name = `[+${item.enhancement}]${item.name}`
+    return { ...item }
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,117 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer', () => {
+    describe('repaire()', () => {
+        it('restores object durrability to 100', () => {
+            const item = {
+                name: 'worn sword',
+                enhancement: 15,
+                durability: 50
+            }
+
+            expect(item.durability).toBe(50)
+
+            enhancer.repair(item)
+
+            expect(item.durability).toBe(100)
+        })
+    })
+    describe('succeed()', () => {
+        it('increases the enhancement', () => {
+            const item = {
+                name: 'worn sword',
+                enhancement: 0,
+                durability: 50
+            }
+            expect(item.enhancement).toBe(0)
+
+            enhancer.succeed(item)
+
+            expect(item.enhancement).toBe(1)
+
+            enhancer.succeed(item)
+            enhancer.succeed(item)
+
+            expect(item.enhancement).toBe(3)
+        })
+        it('doesnt enhance when item enhancement is 20', () => {
+            const item = {
+                name: 'op sword',
+                enhancement: 20,
+                durability: 50
+            }
+
+            expect(item.enhancement).toBe(20)
+            enhancer.succeed(item)
+            expect(item.enhancement).toBe(20)
+        })
+    })
+
+    describe('fail()', () => {
+        it('lowers durability currectly', () => {
+            const item1 = {
+                name: 'op sword',
+                enhancement: 20,
+                durability: 100
+            }
+            const item2 = {
+                name: 'op sword',
+                enhancement: 0,
+                durability: 100
+            }
+
+            expect(item1.durability).toBe(100)
+            expect(item2.durability).toBe(100)
+            enhancer.fail(item1)
+            enhancer.fail(item2)
+            expect(item1.durability).toBe(90)
+            expect(item2.durability).toBe(95)
+        })
+        it('lowers items with an enhancement over 14 by 1', () => {
+            const item1 = {
+                name: 'op sword',
+                enhancement: 20,
+                durability: 100
+            }
+            const item2 = {
+                name: 'op sword',
+                enhancement: 15,
+                durability: 100
+            }
+
+            expect(item1.enhancement).toBe(20)
+            expect(item2.enhancement).toBe(15)
+            enhancer.fail(item1)
+            enhancer.fail(item2)
+            expect(item1.enhancement).toBe(19)
+            expect(item2.enhancement).toBe(15)
+            enhancer.fail(item1)
+            enhancer.fail(item1)
+            enhancer.fail(item1)
+            expect(item1.enhancement).toBe(16)
+        })
+    })
+    describe('get()', () => {
+        it('doesnt update items with 0 enhancement', () => {
+            const item = {
+                name: 'op sword',
+                enhancement: 0,
+                durability: 100
+            }
+            expect(item.name).toBe(`op sword`)
+            enhancer.get(item)
+            expect(item.name).toBe(`op sword`)
+        })
+        it('updates item name to the correct value', () => {
+            const item = {
+                name: 'op sword',
+                enhancement: 1,
+                durability: 100
+            }
+            expect(item.name).toBe(`op sword`)
+            enhancer.get(item)
+            expect(item.name).toBe(`[+1]op sword`)
+        })
+    })
+})
